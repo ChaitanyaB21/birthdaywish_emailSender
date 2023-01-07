472ecfeb59d093939a6dc35ee4ed927886b7f91e
ChaitanyaB21 <113889111+ChaitanyaB21@users.noreply.github.com>
2023-01-07 19:56:50 +0530

    Update App.js

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'ghodmare.1@iitj.ac.in',
-        pass: 'Chait@123'
+        pass: TEXT
     }
 });
 
